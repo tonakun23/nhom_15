1e3912e39df4e301948db3753a8208578a88c12f
toan <nguyenvantoanmnsh@gmail.com>
2024-05-07 19:14:17 +0700

    sua header js va them trang sanpham

--- mypham/assets/js/header.js
+++ mypham/assets/js/header.js
@@ -17,3 +17,33 @@ document.querySelector('.search').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active2')
 }
+
+
+
+
+function toggleSubnav(event) {
+    var subnav = event.target.nextElementSibling;
+    subnav.style.display = (subnav.style.display === "none" || subnav.style.display === "") ? "block" : "none";
+}
+
+var sanphamLinks = document.getElementsByClassName("sanpham");
+for (var i = 0; i < sanphamLinks.length; i++) {
+    sanphamLinks[i].addEventListener("dblclick", function(event) {
+        if (event.target.tagName === "A") {
+            window.location.href = event.target.getAttribute("href");
+        }
+    });
+}
+
+var lastClickTime = 0;
+
+function toggleSubnav(event) {
+    var currentTime = new Date().getTime();
+    if (currentTime - lastClickTime < 300) { // Check if the time between clicks is less than 300 milliseconds (0.3 seconds)
+        window.location.href = "sanpham.html";
+    } else {
+        var subnav = event.target.nextElementSibling;
+        subnav.style.display = (subnav.style.display === "none" || subnav.style.display === "") ? "block" : "none";
+    }
+    lastClickTime = currentTime;
+}
